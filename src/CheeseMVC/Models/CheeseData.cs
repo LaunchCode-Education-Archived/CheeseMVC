04dc527c6407e51ae1bd1ca072f5093c8c680418
Scott Land <101.scott@gmail.com>
2017-03-21 23:21:13 -0500

    Changes from Models and exercise.

---
+++ src/CheeseMVC/Models/CheeseData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class CheeseData
+    {
+        static private List<Cheese> cheeses = new List<Cheese>();
+
+        public static List<Cheese> GetAll()
+        {
+            return cheeses;
+        }
+
+        public static void Add(Cheese newCheese)
+        {
+            // Add the new cheese to my existing cheeses
+            cheeses.Add(newCheese);
+        }
+
+        public static void Remove(int id)
+        {
+            Cheese cheeseToRemove = GetById(id);
+            cheeses.Remove(cheeseToRemove);
+        }
+
+        public static Cheese GetById(int id)
+        {
+            return cheeses.Single(x => x.CheeseId == id);
+        }
+    }
+}
