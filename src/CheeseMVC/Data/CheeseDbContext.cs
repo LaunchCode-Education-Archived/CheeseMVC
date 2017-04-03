c4e247cee7cef979c94110ce62cc1730a6e10d68
Chris Bay <chris@chrisbay.net>
2017-04-03 06:32:23 -0500

    Prep lesson

---
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CheeseMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheeseMVC.Data
+{
+    public class CheeseDbContext : DbContext
+    {
+        public DbSet<Cheese> Cheeses { get; set; }
+
+        public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
+            : base(options)
+        { }
+
+    }
+}
--- src/CheeseMVC/Startup.cs
+++ src/CheeseMVC/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using CheeseMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheeseMVC
 {
@@ -27,6 +29,10 @@ namespace CheeseMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<CheeseDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             // Add framework services.
             services.AddMvc();
         }
